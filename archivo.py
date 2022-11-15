c7d6dfd4d47c39e583af36d59447e6265b19d776
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-14 19:04:49 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -3,5 +3,6 @@ import pandas as pd
 import numpy as np
 st.title('Datos aleatorios')
 n = st.slider("n", 5,100, step=1)
+df = pd.read_excel('wine.xlsx')
 chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-st.line_chart(chart_data)
+st.line_chart(df)
