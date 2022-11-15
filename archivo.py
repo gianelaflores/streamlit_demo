7b4d79da3331d4c690b2b6c42f87feed1d6e498b
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-14 19:09:01 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Datos aleatorios')
+
 @st.experimental_memo
 def download_data():
-  url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download '
-  filename = 'data.csv'
+  url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download'
+  filename = 'positivos_covid.csv'
   urllib.request.urlretrieve(url, filename)
 download_data()
