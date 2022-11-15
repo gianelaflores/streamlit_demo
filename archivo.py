9dfd200f228eb8ba5367695a56301948093907e7
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-14 19:07:36 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Datos aleatorios')
-n = st.slider("n", 5,100, step=1)
-df = pd.read_excel('wine.xlsx')
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-st.line_chart(df)
+@st.experimental_memo
+def download_data():
+  url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download '
+  filename = 'data.csv'
+  urllib.request.urlretrieve(url, filename)
+download_data()
