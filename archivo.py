bd9d0540a9a5560d236cdfe35518f55094d850bd
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-14 19:10:46 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -7,6 +7,6 @@ st.title('Datos aleatorios')
 @st.experimental_memo
 def download_data():
   url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download'
-  filename = 'positivos_covid.csv'
+  filename = 'data.csv'
   urllib.request.urlretrieve(url, filename)
 download_data()
