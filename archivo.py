dc8f4125e92b1ec6180fc20ac15b40e340f49403
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-17 14:08:55 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 
 cols = ['Date','Price','Open','High','Low','Vol.','Change %']
-url = 'https://github.com/PeterTXS09/streamlit_demo/blob/main/USD_PEN%20Historical%20Data2.csv'
+url = 'https://raw.githubusercontent.com/PeterTXS09/streamlit_demo/main/USD_PEN%20Historical%20Data2.csv'
 datos = pd.read_csv(url, header=None, sep=',', names=cols, error_bad_lines=False)
 
 st.bar_chart(datos['Price'])
