25d2c0a6c11f69c9182700d22d667f9eb154535f
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-17 15:39:03 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -2,9 +2,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-# cols = ['Date','Price','Open','High','Low','Vol.','Change %']
+st.title('Precio del dólar')
 url = 'https://raw.githubusercontent.com/PeterTXS09/streamlit_demo/main/USD_PEN%20Historical%20Data2.csv'
-# datos = pd.read_csv(url, header=None, sep=',', names=cols, error_bad_lines=False)
 datos = pd.read_csv(url, sep=',')
 
 st.line_chart(data=datos, x='Date', y='Price')
