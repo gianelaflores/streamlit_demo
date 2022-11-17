59512b58d72c2c2aa6857cb0cde992449e2d713d
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-17 13:49:36 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -4,8 +4,9 @@ import numpy as np
 
 @st.experimental_memo
 def download_data():
+    cols = ['Date','Price','Open','High','Low','Vol.','Change %']
     url = 'https://github.com/PeterTXS09/streamlit_demo/blob/main/USD_PEN%20Historical%20Data.csv'
-    df = pd.read_csv(url, delimiter=",")
+    df = pd.read_csv(url, header=None, delimiter=',', names=cols)
     return df
 
 datos = download_data()
