ff677ec738c552ee61d90be0ecd5b8a45a3d5d5f
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-17 15:26:50 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -6,4 +6,4 @@ cols = ['Date','Price','Open','High','Low','Vol.','Change %']
 url = 'https://raw.githubusercontent.com/PeterTXS09/streamlit_demo/main/USD_PEN%20Historical%20Data2.csv'
 datos = pd.read_csv(url, header=None, sep=',', names=cols, error_bad_lines=False)
 
-st.bar_chart(datos['Price'])
+st.line_chart(datos['Price'])
