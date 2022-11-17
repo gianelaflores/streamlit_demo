d7ae51217b43bbe977724ffd3cefcdc267ce43ab
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-17 15:33:32 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -5,5 +5,7 @@ import numpy as np
 cols = ['Date','Price','Open','High','Low','Vol.','Change %']
 url = 'https://raw.githubusercontent.com/PeterTXS09/streamlit_demo/main/USD_PEN%20Historical%20Data2.csv'
 datos = pd.read_csv(url, header=None, sep=',', names=cols, error_bad_lines=False)
+datos['Price'] = pd.to_numeric(datos['Price'])
+
 
 st.line_chart(data=datos, x='Date', y='Price')
