7e8599912d4b6d168f9efb9b39fb61bfeb0ed66c
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-17 14:05:21 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -2,12 +2,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-@st.experimental_memo
-def download_data():
-    cols = ['Date','Price','Open','High','Low','Vol.','Change %']
-    url = 'https://github.com/PeterTXS09/streamlit_demo/blob/main/USD_PEN%20Historical%20Data2.csv'
-    df = pd.read_csv(url, header=None, sep=',', names=cols, lineterminator='\n')
-    return df
+cols = ['Date','Price','Open','High','Low','Vol.','Change %']
+url = 'https://github.com/PeterTXS09/streamlit_demo/blob/main/USD_PEN%20Historical%20Data2.csv'
+datos = pd.read_csv(url, header=None, sep=',', names=cols, lineterminator='\n')
 
-datos = download_data()
 st.bar_chart(datos['Price'])
