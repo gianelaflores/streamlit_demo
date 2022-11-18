005a1b5d1016d161b441b2bddbde13f79291739e
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-18 08:03:06 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 
 url = 'https://raw.githubusercontent.com/PeterTXS09/streamlit_demo/main/USD_PEN%20Historical%20Data2.csv'
 datos = pd.read_csv(url, sep=',')
 
-
 st.title('Precio del dólar')
 st.write('Analicemos el precio del dólar a lo largo del perido')
-#st.pyplot(data=datos, x='Date', y='Price')
+st.line_chart(data=datos, x='Date', y='Price')
 
 st.title('Precio del dólar en el día - valor más alto y bajo')
 st.write('Analicemos el precio del dólar a lo largo del perido establecido vs el valor más alto y más bajo')
