b4268cff1dff73a59ebef2a30629a41e94de2cba
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-18 07:30:31 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -14,6 +14,4 @@ st.title('Precio del dólar en el día - valor más alto y bajo')
 st.write('Analicemos el precio del dólar a lo largo del perido establecido vs el valor más alto y más bajo')
 st.line_chart(data=datos, x='Date', y=['Price', 'High', 'Low'])
 
-st.title('Precio del dólar en volúmenes movidos')
-st.write('Precio del dólar con respecto al día anterior')
-st.line_chart(data=datos, x='Date', y='Change')
+
