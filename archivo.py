595ade7e6439b92cf84b59855cbcae9a80557f11
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-18 07:45:39 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -8,10 +8,16 @@ datos = pd.read_csv(url, sep=',')
 
 st.title('Precio del dólar')
 st.write('Analicemos el precio del dólar a lo largo del perido')
-st.line_chart(data=datos, x='Date', y='Price')
+fig, ax = plt.subplots()
+precio = datos['Price']
+ax.hist(precio)
+#st.pyplot(data=datos, x='Date', y='Price')
+st.pyplot(precio)
 
 st.title('Precio del dólar en el día - valor más alto y bajo')
 st.write('Analicemos el precio del dólar a lo largo del perido establecido vs el valor más alto y más bajo')
 st.line_chart(data=datos, x='Date', y=['Price', 'High', 'Low'])
 
-
+st.title('Precio del dólar en volúmenes movidos')
+st.write('Precio del dólar con respecto al día anterior')
+st.line_chart(data=datos, x='Date', y='Change')
