6eb27dbd0897e8ac92e01629715dad095c735990
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2022-11-28 18:07:54 -0500

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -7,20 +7,25 @@ url = 'https://raw.githubusercontent.com/PeterTXS09/streamlit_demo/main/USD_PEN%
 datos = pd.read_csv(url, sep=',')
 
 st.title('Precio del dólar')
+st.header('Precio del dólar a lo largo del tiempo')
 st.write('Analicemos el precio del dólar a lo largo del perido')
 st.line_chart(data=datos, x='Date', y='Price')
 
-st.title('Precio del dólar en el día - valor más alto y bajo')
+st.header('Precio del dólar en el día - valor más alto y bajo')
 st.write('Analicemos el precio del dólar a lo largo del perido establecido vs el valor más alto y más bajo')
 st.line_chart(data=datos, x='Date', y=['Price', 'High', 'Low'])
 
 
-st.title('Ejemplo de mapa')
+st.header('Ejemplo de mapa')
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 st.map(df)
 
+st.header('tabla de datos')
+st.table(df)
+
+st.header('Ejemplo')
 chart_data = pd.DataFrame(
    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
    columns=['lat', 'lon'])
